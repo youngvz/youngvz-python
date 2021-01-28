c9b00a6da2f28218e3da28b1d2c63f403393db7a
youngvz <shah.a.viraj@gmail.com>
2021-01-28 08:15:46 -0500

    Added CORS support for flask app

--- flask_example.py
+++ flask_example.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
+from flask import jsonify
 
 app = Flask(__name__)
+CORS(app)
 
 """ This route is answers to the GET requests and returns back a welcome string """
 @app.route("/")
@@ -10,4 +13,4 @@ def welcome():
 """ This route is answers to the POST requests and returns back a greeting string """
 @app.route("/greet", methods=['POST'])
 def greet():
-    return 'Hi Viraj'
+    return jsonify('Hi Viraj')
