ef64b1ba3de4f8d3c27bf0a9cbaef5b16f59fba2
youngvz <shah.a.viraj@gmail.com>
2021-01-28 15:23:19 -0500

    Created Requirements.txt and updated flask_example.py to include more routes

--- flask_example.py
+++ flask_example.py
@@ -5,6 +5,9 @@ from flask import jsonify
 app = Flask(__name__)
 CORS(app)
 
+global count
+count = 0
+
 """ This route is answers to the GET requests and returns back a welcome string """
 @app.route("/")
 def welcome():
@@ -14,3 +17,23 @@ def welcome():
 @app.route("/greet", methods=['POST'])
 def greet():
     return jsonify('Hi Viraj')
+
+
+""" This route is answers to the GET requests and returns back the count """
+@app.route("/count", methods=['GET'])
+def getCount():
+    return jsonify({'count': count})
+
+""" This route is answers to the POST requests and increments the count """
+@app.route("/count", methods=['POST'])
+def increment():
+    global count
+    count += 1
+    return jsonify({'count': count})
+
+""" This route is answers to the POST requests and resets the count """
+@app.route("/reset", methods=['POST'])
+def reset():
+    global count
+    count = 0
+    return jsonify({'count': count})
